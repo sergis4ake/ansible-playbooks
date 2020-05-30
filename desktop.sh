20d2368647d179fdb1c1556d88e9fac6cc5c3bf8
srg <srg@ssoo>
2020-05-30 20:12:03 +0200

    Update ubuntu xenial to debian buster

--- desktop.sh
+++ desktop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-time ansible-playbook -vvvv -c local site.yml -K --become-method=sudo --tags desktop -i hosts --extra-vars "manager"
+time ansible-playbook -vvvv -c local site.yml -K --become-method=sudo --tags "desktop" -i hosts --extra-vars "manager"
+
